fd62e515222deae26e2df496114717fab8d9f569
GregSimo99 <36053683+GregSimo99@users.noreply.github.com>
2020-01-13 10:20:23 +0100

    nuovo leader

--- header.h
+++ header.h
@@ -1,28 +1,45 @@
 #include<iostream>
 #include<vector>
 
+using namespace std;
+
 class Componente
 {
 public:
-	Componente();
-	~Componente();
-	//get e set se servono
+    Componente(){}
+    ~Componente(){}
+    //get e set se servono
 private:
-	int id, d_time, quantita, arrivo;
-	std::string nome;
-	double[] price;
+    int id, d_time, quantita, arrivo;
+    std::string nome;
+    double price[];
 };
 
 class Elettrodomestico
 {
 public:
-	Elettrodomestico();
-	~Elettrodomestico();
+    Elettrodomestico(){}
+    ~Elettrodomestico(){}
 
 private:
-	int id, time_stamp, stato, quantita;
-	std::vector<Componente> componenti;
-	std::string nome;
-	double price;
+    int id, time_stamp, stato, quantita;
+    std::vector<Componente> componenti;
+    std::string nome;
+    double price;
 };
 
+class Azienda
+{
+public:
+    Azienda();
+    ~Azienda();
+
+    //funzioni per leggere i dati
+    void lettura_elettrodomestici();
+private:
+    int cassa, mese;
+    vector<Elettrodomestico> catalogo;
+    vector<Elettrodomestico> ordini;
+    vector<Componente> magazzino;
+    vector<Componente> cAttesa;
+};
