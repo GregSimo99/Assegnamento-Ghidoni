1f48886fc0facde36051cbab75a7c6fd45df4cc8
perini98 <59748485+perini98@users.noreply.github.com>
2020-01-23 20:56:05 +0100

    Add files via upload

--- Sorgente2.cpp
+++ Sorgente2.cpp
@@ -1,4 +1,4 @@
-//Perini Mattia
+//Perini Mattia	  1162399
 
 #include <iostream>
 #include <stdlib.h>
@@ -6,24 +6,37 @@
 
 using namespace std;
 
+
 int Azienda::prox_mese() {
 	++mese; //mese successivo
 	_sleep(1500); //attesa di 1.5 secondi per simulare il trascorrere dei mesi
-	cout << "--------  " << mese << "° mese  --------" << endl;
+	cout << "----------  " << mese << "° mese  ----------" << endl;
 
-	/*for (int i = 0; i < cAttesa.size(); ++i) { //decremento di un mese il tempo di attesa dei componenti
-		Componente c = cAttesa[i];
-		if (c.get_arrivo() != 0) --c.get_arrivo;
-		else
-			controllo_arrivi(); //quando arriva a 0 il componente è arrivato
-	}*/
 	return mese;
 }
 
-void produzione() {
 
+void Azienda::produzione() { //Legge i componenti necessari per la produzione
+	for (int i = 0; i < ordiniP.size(); ++i) {
+		vector<Componente> comp = ordiniP[i].getComp(); //vettore che contiene i componenti di ogni elettrodomestico da produrre
+
+		for (int k = 0, k < comp.size(), ++k) {
+			int id = comp[k].get_id();
+			int quantita = comp[k].get_quantita();
+
+			ricerca_comp(id, quantita);
+		}
+		
+	}
 }
 
-bool ricerca_comp(int id) { //Controlla i componenti in magazzino per l'ordine
-	 
+
+bool Azienda::ricerca_comp(int id, int quantita) { //Controlla se ci sono i componenti necessari per produrre l'ordine
+
+	for (int i = 0; i < magazzino.size(); ++i) {
+		Componente comp = magazzino[i];
+
+			if (id == comp.get_id() && quantita == comp.get_quantita()) return true;
+			else ordina_comp(id, quantita);
+	}
 }
