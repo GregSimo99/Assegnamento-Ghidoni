4d63d02cfedf8aba9720ed9055a42144f7e083ae
perini98 <59748485+perini98@users.noreply.github.com>
2020-01-26 12:04:17 +0100

    Add files via upload

--- Sorgente2.cpp
+++ Sorgente2.cpp
@@ -7,8 +7,9 @@
 using namespace std;
 
 
-int Azienda::prox_mese() {
-	++mese; //mese successivo
+int Azienda::prox_mese() 
+{
+	++mese;
 	_sleep(1500); //attesa di 1.5 secondi per simulare il trascorrere dei mesi
 	cout << "----------  " << mese << "° mese  ----------" << endl;
 
@@ -16,27 +17,52 @@ int Azienda::prox_mese() {
 }
 
 
-void Azienda::produzione() { //Legge i componenti necessari per la produzione
-	for (int i = 0; i < ordiniP.size(); ++i) {
-		vector<Componente> comp = ordiniP[i].getComp(); //vettore che contiene i componenti di ogni elettrodomestico da produrre
+void Azienda::produzione() //legge i componenti necessari per la produzione
+{ 
+	for (int i = 0; i < ordiniP.size(); ++i) 
+	{
+		if (!ordiniP[i].statoOrdine()) //stato = false, ordine non ancora processato (giusto?)
+		{
+			vector<Componente> comp = ordiniP[i].getComp(); //vettore che contiene i componenti di ogni elettrodomestico da produrre
+			int k = ordiniP[i].getQ(); //k contiene la quantità degli elettrodomestici da produrre
 
-		for (int k = 0, k < comp.size(), ++k) {
-			int id = comp[k].get_id();
-			int quantita = comp[k].get_quantita();
+			bool evadere = true;
+			for (int j = 0, j < comp.size(), ++j)
+			{
+				int id = comp[j].get_id();
+				int quantita = k*comp[j].get_quantita(); //k dipende dal numero di elettrodomestici da produrre
 
-			ricerca_comp(id, quantita);
+				bool esito = ricerca_comp(id, quantita); //true se trovato il componente, false altrimenti
+
+				if (!esito) //se non è né in magazzino né in arrivo, da ordinare
+				{
+					ordina_comp(id, quantita);
+					evadere = false; //se manca un solo componente non si può evadere l'ordine
+				}				
+			}
+
+			bool ordiniP[i].statoOrdine() = true; //ordine processato (non mi ricordo bene a cosa serviva lo stato, è giusto qua e scritto così?)
+
+			if (evadere) evasione_ordine(ordiniP[i].get_id()); //evasione dell'ordine se ci sono tutti i componenti necessari
 		}
 		
 	}
 }
 
 
-bool Azienda::ricerca_comp(int id, int quantita) { //Controlla se ci sono i componenti necessari per produrre l'ordine
+bool Azienda::ricerca_comp(int id, int quantita) //controlla se ci sono i componenti necessari per produrre l'ordine
+{ 
 
-	for (int i = 0; i < magazzino.size(); ++i) {
-		Componente comp = magazzino[i];
+	for (int i = 0; i < magazzino.size(); ++i) 
+	{
 
-			if (id == comp.get_id() && quantita == comp.get_quantita()) return true;
-			else ordina_comp(id, quantita);
+		Componente comp_mag = magazzino[i]; //componenti del magazzino
+		Componente comp_arr = cAttesa[i]; //componenti in arrivo
+
+		if (id == comp_mag.get_id() && quantita == comp_mag.get_quantita()) return true //componente trovato in magazzino
+			
+		else if (id == comp_arr.get_id() && quantita == comp_arr.get_quantita()) return true //componente in arrivo
+
+		return false
 	}
 }
