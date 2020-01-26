a2cc097767a9623335a8727423e439aa8361145c
perini98 <59748485+perini98@users.noreply.github.com>
2020-01-26 21:17:26 +0100

    Add files via upload

--- Sorgente2.cpp
+++ Sorgente2.cpp
@@ -19,27 +19,43 @@ int Azienda::prox_mese()
 
 void Azienda::produzione() //legge i componenti necessari per la produzione
 { 
-	for (int i = 0; i < ordiniP.size(); ++i) 
+	for (int i = 0; i < ordiniP.size(); i++)
 	{
-		if (!ordiniP[i].stato_ordine()) //stato = false, ordine non ancora processato
+		Elettrodomestico& el = ordiniP[i];
+		if (el.getStato()) //se è stato prodotto l'elettrodomestico
 		{
-			Elettrodomestico el = ordiniP[i];
-			vector<Componente> comp = el.getComp(); //vettore che contiene i componenti di ogni elettrodomestico da produrre
-			int k = ordiniP[i].getQ(); //k contiene la quantità degli elettrodomestici da produrre
+			evasi.push_back(Ordine(el.getId(), 0, el.getQ(), 0));
+			ordiniP.erase(ordiniP.begin() + i);
+		}
 
-			bool produrre = true; //rimane vera se ci sono tutti i componenti necessari
-			for (int j = 0, j < comp.size(), ++j)
+		else //ordine non ancora processato
+		{
+			vector<Componente_richiesto>& comp = el.getComp(); //vettore che contiene i componenti di ogni elettrodomestico da produrre
+			int k = el.getQ(); //k contiene la quantità degli elettrodomestici da produrre
+
+			bool evadere = true;
+			for (int j = 0, j < comp.size(), j++)
 			{
 				int id = comp[j].get_id();
-				int quantita = k*comp[j].get_quantita();
+				int quantita = k * comp[j].get_quantita();
 
 				bool esito = ricerca_comp(id, quantita); //true se trovato il componente, false altrimenti
 
 				if (!esito) //se non è in magazzino è da ordinare
 				{
-					ordina_comp(id, quantita);
+					if (!comp[j].getStato())
+					{
+						bool ordinato = ordina_comp(id, quantita);
+						if (ordinato) comp[j].setStato(true);
+					}
 					evadere = false; //se manca un solo componente non si può evadere l'ordine
+					
 				}				
+			}
+			if (evadere)
+			{
+				e.setStato(true);
+				for (int i = 0; i < comp.size[]; i++) tolgo_magazzino(comp[i].get_id(), k * comp[i].get_quantita());
 			}
 
 			ordiniP[i].stato_ordine() = true; //ordine processato
@@ -53,7 +69,7 @@ void Azienda::produzione() //legge i componenti necessari per la produzione
 bool Azienda::ricerca_comp(int id, int quantita) //controlla se ci sono i componenti necessari per produrre l'ordine
 { 
 
-	for (int i = 0; i < magazzino.size(); ++i) 
+	for (int i = 0; i < magazzino.size(); i++)
 	{
 
 		Componente comp = magazzino[i]; //componenti del magazzino
